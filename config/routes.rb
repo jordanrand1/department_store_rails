3b11b5f2850cbad333796946dc6765a72a5c9764
Jordan Rand <jorrand@gmail.com>
2018-09-04 15:21:49 -0600

    init with department model and controller

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'departments/index'
+  get 'departments/new'
+  get 'departments/edit'
+  get 'departments/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
