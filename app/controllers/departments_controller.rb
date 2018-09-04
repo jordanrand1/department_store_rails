d6bfd9b6374eb93ab5c1ce3b5513c5baaaab3af9
Jordan Rand <jorrand@gmail.com>
2018-09-04 16:11:53 -0600

    added views and crud to controller

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,13 +1,37 @@
 class DepartmentsController < ApplicationController
   def index
+    @departments = Department.all
   end
 
   def new
+    @department = Department.new
+  end
+
+  def create
+    @department = Department.new(department_params)
+    
+    if @department.save
+      redirect_to departments_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @department.update(department_params)
+    redirect_to departments_path
   end
 
   def show
   end
+
+  private
+
+    def get_department
+      @department = Department.find(params[:id])
+    end
+
+    def department_params
+      params.require(:department).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'departments/index'
-  get 'departments/new'
-  get 'departments/edit'
-  get 'departments/show'
+  resources :departments
+
+  root 'departments#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
